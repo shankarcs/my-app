6d54d85b2b3b25965d39fedcd8778763836a2149
shankarcs <47312704+shankarcs@users.noreply.github.com>
2020-06-19 12:56:33 +0530

    Update generate_python_licenses.sh

--- generate_python_licenses.sh
+++ generate_python_licenses.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 licenses_file="$1"
+repo_linkname="$2"
 set -ex
 pip-licenses --with-system --with-authors --with-urls --format=csv --output-file=python_license.csv
 awk -F',' '{ print $1 "," $2 "," $3 "," $4 "," $5}' python_license.csv | sort -u  >> "${licenses_file}"
+sed 's/$/,"${repo_linkname}"/' "${licenses_file}"
 sed -i 's/"//g' "${licenses_file}"
