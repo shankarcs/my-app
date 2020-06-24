5975d2cf02e3b93a270ea7ae1833e2d213d5f01c
shankarcs <47312704+shankarcs@users.noreply.github.com>
2020-06-24 16:02:04 +0530

    Update pythonrepolink.sh

--- pythonrepolink.sh
+++ pythonrepolink.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 licenses_file="$1"
 repo_linkname="$2"
-test="$3"
+repo_dependency="$3"
 set -ex
-awk -F "," '{print $1 "," $2 "," $3 "," $4 "," $5 "," $6 "'"$repo_linkname"'" }' "${licenses_file}" | sort -u  >> "${test}"
-sed -i 's/"//g' "${test}"
+awk -F "," '{print $1 "," $2 "," $3 "," $4 "," $5 "," $6 "'"$repo_linkname"'" }' "${licenses_file}" | sort -u  >> "${repo_dependency}"
+sed -i 's/"//g' "${repo_dependency}"
