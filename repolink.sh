5a9f3c4ca756e56e8157993ed7c5d6bf1289b855
shankarcs <47312704+shankarcs@users.noreply.github.com>
2020-06-24 10:32:52 +0530

    Update repolink.sh

--- repolink.sh
+++ repolink.sh
@@ -2,5 +2,5 @@
 licenses_file="$1"
 repo_linkname="$2"
 set -ex
-awk -F',' '{print $1 "," $2 "," $3 "," $4 "," $5 "," $6 "," $7 "'"${repo_link}"'" }' intermediate.csv "${licenses_file}"
+awk -F',' '{print $1 "," $2 "," $3 "," $4 "," $5 "," $6 "," $7 "'"${repo_linkname}"'" }' intermediate.csv "${licenses_file}"
 sed -i 's/"//g' "${licenses_file}"
