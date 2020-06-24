cdfec6256f8e3f4a25a38e15a5310241c9f0f642
shankarcs <47312704+shankarcs@users.noreply.github.com>
2020-06-24 10:38:59 +0530

    Update pythonrepolink.sh

--- pythonrepolink.sh
+++ pythonrepolink.sh
@@ -2,5 +2,5 @@
 licenses_file="$1"
 repo_linkname="$2"
 set -ex
-awk -F "," '{print $1 "," $2 "," $3 "," $4 "," $5 "," $NF "'"$repo_linkname"'" }' "${licenses_file}"
+awk -F "," '{print $1 "," $2 "," $3 "," $4 "," $5 "," $6 "'"$repo_linkname"'" }' "${licenses_file}"
 sed -i 's/"//g' "${licenses_file}"
