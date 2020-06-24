582022e13014e59996e2fb07d2c9d921e12f9c3d
shankarcs <47312704+shankarcs@users.noreply.github.com>
2020-06-24 10:31:46 +0530

    Create repolink.sh

---
+++ repolink.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+licenses_file="$1"
+repo_linkname="$2"
+set -ex
+awk -F',' '{print $1 "," $2 "," $3 "," $4 "," $5 "," $6 "," $7 "'"${repo_link}"'" }' intermediate.csv "${licenses_file}"
+sed -i 's/"//g' "${licenses_file}"
