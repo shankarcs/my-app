2348dc94a21d889ea880d2dc415ac71bff1c3f83
shankarcs <47312704+shankarcs@users.noreply.github.com>
2020-06-30 12:26:38 +0530

    Update GitHubTasks.py

--- GitHubTasks.py
+++ GitHubTasks.py
@@ -671,7 +671,7 @@ parser_e.set_defaults(func=clone_repo)
 args = parser.parse_args()
 
 # Initialize common static variables
-_filetrek_url = "https://api.github.com/users/shankarcs/repos"
+_filetrek_url = "https://github.com/shankarcs"
 _authorization_token = 'token ' + args.key
 _headers = {'Authorization': _authorization_token}
 _work_dir = Path(args.reponame)
